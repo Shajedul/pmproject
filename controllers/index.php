ecfa000968ef0ddf5a86c0b4a2f8b24463cd7122
Shajedul <helloitsshuvo@gmail.com>
2018-12-20 23:36:03 +0600

    Created Nested conditions for login credential verification. Code inside the condtions has not been written yet.

--- controllers/index.php
+++ controllers/index.php
@@ -2,14 +2,22 @@
 
 require 'view/login.view.php';
 //var_dump($app['database']->selectAll());
-//$uname ='not set yet';
-$pwd =null;
 if (isset($_POST['submit']))
 {
-    if ($_POST['uname']!=null)
-    {
-           echo ' if block is working';
 
+    if (!empty($_POST['uname']))
+    {
+        $uname=trim($_POST['uname']);
+
+        if (!empty($_POST['password']))
+        {
+            $pword=trim($_POST['password']);
+            // code for login credential verification
+        }
+        else
+        {
+            echo "Notification for empty password";
+        }
     }
     else
     {
